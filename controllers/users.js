d4d8b2f7031eaa31b81044064222e2efcefd49a7
jones876 <jones111080@yandex.ru>
2023-07-12 21:26:39 +0300

    исправлено замечание автотеста

--- controllers/users.js
+++ controllers/users.js
@@ -18,7 +18,7 @@ module.exports.getUserById = (req, res) => {
       res.status(200).send({ user });
     })
     .catch((err) => {
-      if (err.name === 'ValidationError') {
+      if (err.name === 'CastError') {
         return res
           .status(400)
           .send({ message: 'Переданы некорректные данные' });
