bf86122a33af3f9e25c0968838381a80f7d8b79e
jones876 <jones111080@yandex.ru>
2023-07-12 21:34:49 +0300

    исправлено замечание автотеста

--- controllers/users.js
+++ controllers/users.js
@@ -71,7 +71,8 @@ module.exports.updateAvatar = (req, res) => {
   User.findByIdAndUpdate(req.user._id, { avatar })
     .then((user) => {
       if (!user) {
-        return res.status(404).send({ message: 'Пользователь не найден' });
+        res.status(404).send({ message: 'Пользователь не найден' });
+        return;
       }
       res.status(200).send({ user });
     })
