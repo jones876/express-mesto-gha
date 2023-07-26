25bca4ea1368d23bf9d89482bf008c43e829f837
Jeka <jones111080@yandex.ru>
2023-07-26 19:33:13 +0300

    исправлено замечание автотеста

--- app.js
+++ app.js
@@ -32,10 +32,9 @@ app.post(
   '/signup',
   celebrate({
     body: Joi.object().keys({
-      name: Joi.string().required().min(2).max(30),
-      about: Joi.string().required().min(2).max(30),
+      name: Joi.string().min(2).max(30),
+      about: Joi.string().min(2).max(30),
       avatar: Joi.string()
-        .required()
         .regex(
           /^(http:\/\/|https:\/\/)(www\.)?\w+([.-]?\w+)*(\.\w{2,3})+(\/\S*)?#?$/,
         ),
